4499e2a08f3b4c6f781a055a3dc44fa5a45d26bb
JorjoWasTaken <93548609+JorjoWasTaken@users.noreply.github.com>
2023-11-03 14:52:28 +0100

    small adjustments

--- Program.cs
+++ Program.cs
@@ -13,6 +13,18 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+// This is middleware code for caching stylesheets (so it can render on time and not look ugly) on the development server, it's for testing during development only!
+app.MapWhen(context => context.Request.Path.StartsWithSegments("/css/app.css"), appBuilder =>
+        {
+            appBuilder.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("Cache-Control", "max-age=31536000"); // 1 year
+                context.Response.Headers.Add("Expires", DateTime.UtcNow.AddYears(1).ToString("R"));  
+                await next.Invoke();
+            });
+            appBuilder.UseStaticFiles(); // Serve the CSS file
+        });
+
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
